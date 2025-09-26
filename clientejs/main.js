d56ee17696ab31a98a47551905d14b3931efed52
ellainy <mottaellainy@gmail.com>
2025-09-26 20:13:47 -0300

    Adicionando hero, footer, spinner e melhorando visual

--- clientejs/main.js
+++ clientejs/main.js
@@ -4,43 +4,61 @@ const div = document.getElementById("content");
 const url = 'https://api.jikan.moe/v4/anime?q=';
 
 async function animesAleatorios() {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = "block";
     const promisses = [];
     for (let i = 0; i < 16; i++) {
         promisses.push(fetch('https://api.jikan.moe/v4/random/anime').then(res => res.json()).then(data => data.data));
     }
     animes = await Promise.all(promisses)
     renderizarAnimes(animes);
+    spinner.style.display = "none";
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (!input.value) {
-        animesAleatorios();
-    }
+if (!input.value) {
+    document.addEventListener('DOMContentLoaded', () => {
+    animesAleatorios();
 });
+}
 
 form.addEventListener('submit', async function (event) {
-    const query = input.value.trim().toLowerCase();
-
     event.preventDefault();
+    div.innerHTML = '';
 
-    const response = await fetch(url + query);
+    const query = input.value.trim().toLowerCase();
+    if (!query) {return;}
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = "block";
+
+    const response = await fetch(url + encodeURIComponent(query));
     if (!response.ok) {
         throw new Error("Anime não encontrado!");
     }
     const data = await response.json()
     const animes = data.data
     renderizarAnimes(animes);
-
+    spinner.style.display = "none";
 });
 
 function renderizarAnimes(animes) {
     div.innerHTML = '';
-    animes.forEach(anime => {
+
+    // Filtragem de conteúdo +18, não quero perder pontos por isso.
+    const podeNaoMan = ["Hentai", "R-18", "Nudity"];
+
+    const filtrados = animes.filter(anime => {
+        return !podeNaoMan.some(palavra => {
+            const regex = new RegExp(`\\b${palavra}\\b`, "i");
+            return regex.test(anime.rating || "");
+        });
+    });
+
+    filtrados.forEach(anime => {
         const card = `
-            <div class="col-md-3">
+            <div class="col-sm-12 col-md-6 col-lg-3">
                 <a href="" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#modal-${anime.mal_id}">
                 <div class="card h-100 shadow shadow-lg">
-                    <img src="${anime.images.jpg.image_url}" class="card-img-top" alt="${anime.title}">
+                    <img src="${anime.images.jpg.image_url}" class="card-img-top h-100 object-fit-cover" alt="${anime.title}">
                     <div class="card-body">
                         <h5 class="card-title">${anime.title}</h5>
                         <p class="card-text text-dark">
@@ -60,10 +78,17 @@ function renderizarAnimes(animes) {
                 </div>
                 <div class="modal-body">
                     <p><strong>Synopsis:</strong> ${anime.synopsis}</p>
-                    <p><strong>Year:</strong> ${anime.year}</p>
-                    <p><strong>Rating:</strong> ${anime.rating}</p>
-                    <p><strong>Episodes:</strong> ${anime.episodes}</p>
-                    <p><strong>Score:</strong> ${anime.score}</p>
+                    <div class="d-flex flex-wrap gap-4">
+                        <p class="text-nowrap mb-0"><strong>Rank:</strong> ${anime.rank}</p>
+                        <p class="text-nowrap mb-0"><strong>Year:</strong> ${anime.year}</p>
+                        <p class="text-nowrap mb-0"><strong>Status:</strong> ${anime.status}</p>
+                        <p class="text-nowrap mb-0"><strong>Rating:</strong> ${anime.rating}</p>
+                        <p class="text-nowrap mb-0"><strong>Episodes:</strong> ${anime.episodes}</p>
+                        <p class="text-nowrap mb-0"><strong>Duration:</strong> ${anime.duration}</p>
+                        <p class="text-nowrap mb-0"><strong>Popularity:</strong> ${anime.popularity}</p>
+                        <p class="text-nowrap mb-0"><strong>Score:</strong> ${anime.score}</p>
+                        <a href="${anime.url}"></a>
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
